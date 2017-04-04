43b97a1c6cec6fb0b07e36f7e6fb4df57a34e5ce
IZ <iz@Isaacs-MacBook-Air.local>
2017-04-04 16:11:28 -0400

    Added Collection Cells
    Added Cells to collection view - test image included.

---
+++ Calendize/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  Calendize
+//
+//  Created by Isaac Zhu on 4/4/17.
+//  Copyright © 2017 Isaac Zhu. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellLabel: UILabel!
+}
--- Calendize/DashBoardViewController.swift
+++ Calendize/DashBoardViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
-class DashBoardViewController: UIViewController {
+class DashBoardViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
-    @IBOutlet weak var createNewEvent: UIButton!
+    @IBOutlet weak var createNewEvent: UIButton! 
+    @IBOutlet weak var EventView: UICollectionView!
+    
+    @IBOutlet weak var previous: UIButton!
+    @IBOutlet weak var forward: UIButton!
+    var img = ["test","test","test","test","test","test"]
+    var label = ["01","02","03","04","05","06"]
+    var cells : CustomCell!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +30,34 @@ class DashBoardViewController: UIViewController {
         UIGraphicsEndImageContext()
         self.view.backgroundColor = UIColor(patternImage: image)
         
-        //Round-Edge Button! Why not?
+        //Round-Edged Button! Why not?
         createNewEvent.layer.cornerRadius = 50
+        previous.layer.cornerRadius = 50
+        forward.layer.cornerRadius = 50
+        
+        //
+        EventView.delegate = self
+        EventView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return img.count
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.cellImage.image = UIImage(named: "test")
+        cell.cellImage.contentMode = .scaleAspectFit
+        cell.cellLabel.text = label[indexPath.row]
+        cell.layer.cornerRadius = 50
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 5
+        return cell
     }
 
-
+    
+    @IBAction func createNewEventCell(_ sender: UIButton) {
+    }
 }
 
