a81333c7319b1bfc589c31582ac35da9c1581e79
IZ <iz@Isaacs-MacBook-Air.local>
2017-04-10 17:04:42 -0400

    Blurred Effect + Minor Tweaks
    Implemented Blurred Effect + Tweaks regarding background opacity. The
    result is not as desired but I think we should really just move on for
    now — it looks decent enough for a toy program.

--- Calendize/DetailViewController.swift
+++ Calendize/DetailViewController.swift
@@ -11,17 +11,37 @@ import UIKit
 
 class DetailViewController: UIViewController{
     
+    //popover view
     @IBOutlet weak var detailView: UIView!
+    
+    //action buttons
     @IBOutlet weak var yesButton: UIButton!
     @IBOutlet weak var noButton: UIButton!
+    
+    //title
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    //cover image
+    @IBOutlet weak var coverImage: UIImageView!
+    
+    //description
+    @IBOutlet weak var detailTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.extraLight)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.insertSubview(blurEffectView, at: 0)
+        
         // corner radius
         detailView.layer.cornerRadius = 20
-        yesButton.layer.cornerRadius = 25
-        noButton.layer.cornerRadius = 25
+        yesButton.layer.cornerRadius = 15
+        noButton.layer.cornerRadius = 15
         
         // border
         detailView.layer.borderWidth = 2.0
@@ -31,7 +51,13 @@ class DetailViewController: UIViewController{
         detailView.layer.shadowColor = UIColor.black.cgColor
         detailView.layer.shadowOffset = CGSize(width: 4, height: 4)
         detailView.layer.shadowOpacity = 0.8
-        detailView.layer.shadowRadius = 8    }
+        detailView.layer.shadowRadius = 8
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        detailTextView.setContentOffset(CGPoint.zero, animated: false)
+    }
     
     
     @IBAction func returnToPreviousView(_ sender: UIButton) {
